0218632632285447047d79461fe4ec531fc2b0b1
Sri <sri.chandrasekaran@berkeley.edu>
2023-08-13 23:58:58 -0700

    .

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='hero-homepage'>
         <h1 className="hero-title">Education through experimentation.</h1>
-        <p className="hero-description">Making engaging curriculum accessible to all.</p>
+        <p className="hero-description">Making engaging curriculum <span className="line-break">accessible to all.</span></p>
     </div>
   )
 }
