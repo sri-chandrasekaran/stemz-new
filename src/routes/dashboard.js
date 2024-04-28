8977f4eeb4e59383a311717de145f9165b66c637
aaadit24 <97692709+aaadit24@users.noreply.github.com>
2024-04-28 15:27:24 -0700

    stage

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
+import HeroOther from '../components/HeroOther';
+import Dashbar from '../components/Dashbar';
+import Footer from '../components/Footer';
+import './Dashboard.css'
+import AstronomyImage from '../assets/astronomy.PNG'
+import Coding from '../assets/coding.jpg'
+import Biochemistry from '../assets/biochem.PNG'
+import Chemistry from '../assets/chemistry.jpeg'
+import Circuits from '../assets/circuits.jpg'
+import ES from '../assets/environmentalscience.jpg'
+import Psych from '../assets/psych.jpeg'
+import Stats from '../assets/statistics.jpeg'
+import Zoology from '../assets/zoology.jpg'
 import axios from "axios";
-
 import { useNavigate, Link } from 'react-router-dom';
 
-a
+const dashboard = () => {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  }
   return (
     <div>
         <Navbar />
