4446b437b492af57c3069f112d5e7e77e7ccbc31
aaadit24 <97692709+aaadit24@users.noreply.github.com>
2024-04-28 15:22:59 -0700

    Changes for merging

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import axios from "axios";
-import './SignUpForm.css';
+
 import { useNavigate, Link } from 'react-router-dom';
 
-function dashboard() {
+
   return (
     <div>
         <Navbar />
