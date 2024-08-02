eba2b94c77f0356a1a7daff5f8bf4a75b1b6d77c
sri-chandrasekaran <122851944+sri-chandrasekaran@users.noreply.github.com>
2024-08-01 22:20:47 -0700

    fixed price

--- src/routes/PayPalButton.js
+++ src/routes/PayPalButton.js
@@ -73,7 +73,7 @@ const PaymentComponent = () => {
           return actions.order.create({
             purchase_units: [{
               amount: {
-                value: '0.01'
+                value: '39.95'
               },
               shipping: {
                 address: {
