8dbb2fb36a289c15a2bd893e6ffd18254074028d
sri-chandrasekaran <122851944+sri-chandrasekaran@users.noreply.github.com>
2024-08-04 21:47:14 -0700

    fixed ui problems

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,20 +1,44 @@
+// import React from 'react';
+// import Navbar from '../components/Navbar';
+// import HeroOther from '../components/HeroOther'
+// import Form from '../components/Form'
+// import Footer from '../components/Footer'
+
+// const Contact = () => {
+//   return (
+//     <div>
+//       <Navbar />
+//       <HeroOther overlayText="Contact Us" />
+//       <Form />
+//       <div className="contact-container" />
+//       <div style={{ paddingBottom: '100px' }} />
+//       <Footer />
+//     </div>
+//   )
+// }
+
+// export default Contact
+
+
 import React from 'react';
 import Navbar from '../components/Navbar';
-import HeroOther from '../components/HeroOther'
-import Form from '../components/Form'
-import Footer from '../components/Footer'
-// come back to this, make it look nicer
+import HeroOther from '../components/HeroOther';
+import Form from '../components/Form';
+import Footer from '../components/Footer';
+import './contact.css';
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <HeroOther overlayText="Contact Us" />
-      <Form />
+      <div className="contact-container">
+        <Form />
+      </div>
       <div style={{ paddingBottom: '100px' }} />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
