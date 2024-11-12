7fe3f95d67dee51eb8bd4e330a72738d2018aa3d
sri-chandrasekaran <122851944+sri-chandrasekaran@users.noreply.github.com>
2024-11-12 13:35:24 -0800

    self-paced classes page

--- src/routes/SelfPacedClasses.js
+++ src/routes/SelfPacedClasses.js
@@ -30,7 +30,7 @@ const SelfPacedClasses = () => {
                     <img src={AstronomyImage} alt="Astronomy" className="img-course"/>
                     <h1>Astronomy</h1>
                     <p>In this course we will learn about galaxies, the universe, constellations and much more!</p>
-                    <Link to="./astronomy" onClick={scrollToTop}>
+                    <Link to="/astronomy" onClick={scrollToTop}>
                     <button className="cta-button courses-button" style={{marginTop:'93px'}}>Access Lessons</button>
                     </Link>
                 </div>
@@ -38,7 +38,7 @@ const SelfPacedClasses = () => {
                     <img src={Coding} alt="Coding" className="img-course"/>
                     <h1>Basics of Coding</h1>
                     <p>In this course we will learn about movement, variables, conditional statements and many more, using Scratch. No prior experience is needed!</p>
-                    <Link to="./basics-of-coding" onClick={scrollToTop}>
+                    <Link to="/basics-of-coding" onClick={scrollToTop}>
                     <button className="cta-button courses-button">Access Lessons</button>
                     </Link>
                 </div>
@@ -46,7 +46,7 @@ const SelfPacedClasses = () => {
                     <img src={Biochemistry} alt="Biochemistry" className="img-course"/>
                     <h1>Biochemistry</h1>
                     <p>In this course we will learn about molecules, atoms, proteins and more; we encourage the completion of the Chemistry course prior! Parent supervision is needed.</p>
-                    <Link to="./biochemistry" onClick={scrollToTop}>
+                    <Link to="/biochemistry" onClick={scrollToTop}>
                     <button className="cta-button courses-button">Access Lessons</button>
                     </Link>
                 </div>
@@ -56,7 +56,7 @@ const SelfPacedClasses = () => {
                     <img src={Chemistry} alt="chemistry" className="img-course"/>
                     <h1>Chemistry</h1>
                     <p>In this course, your child will learn about matter, energy, and chemical reactions. The course culminates in a final project that serves as a launching pad to inspire your child to learn more! Parent supervision needed.</p>
-                    <Link to="./chemistry" onClick={scrollToTop}>
+                    <Link to="/chemistry" onClick={scrollToTop}>
                     <button className="cta-button courses-button">Access Lessons</button>
                     </Link>
                 </div>
@@ -64,7 +64,7 @@ const SelfPacedClasses = () => {
                     <img src={Circuits} alt="Circuits" className="img-course"/>
                     <h1>Circuits</h1>
                     <p>In this course, your child will learn about the basics of circuits and how they are used in everyday items. </p>
-                    <Link to="./circuits" onClick={scrollToTop}>
+                    <Link to="/circuits" onClick={scrollToTop}>
                     <button className="cta-button courses-button" style={{marginTop:'93px'}}>Access Lessons</button>
                     </Link>
                 </div>
@@ -73,7 +73,7 @@ const SelfPacedClasses = () => {
                     <h1>Environmental</h1>
                     <h1>Science</h1>
                     <p>In this course, we will learn about biodiversity, the cycles of the earth, pollution, recycling, and more!</p>
-                    <Link to="./environmental-science" onClick={scrollToTop}>
+                    <Link to="/environmental-science" onClick={scrollToTop}>
                     <button className="cta-button courses-button">Access Lessons</button>
                     </Link>
                 </div>
@@ -83,7 +83,7 @@ const SelfPacedClasses = () => {
                     <img src={Psych} alt="Psychology" className="img-course"/>
                     <h1>Psychology</h1>
                     <p>In this course, we will learn about biodiversity, the cycles of the earth, pollution, recycling, and more!</p>
-                    <Link to="./psychology" onClick={scrollToTop}>
+                    <Link to="/psychology" onClick={scrollToTop}>
                     <button className="cta-button courses-button" style={{marginBottom:'90px'}}>Access Lessons</button>
                     </Link>
                 </div>
@@ -91,7 +91,7 @@ const SelfPacedClasses = () => {
                     <img src={Stats} alt="Statistics" className="img-course"/>
                     <h1>Statistics</h1>
                     <p>In this course, we will dive into different subtopics of statistics, such as fractions & percents, graphing, and real world-applications.</p>
-                    <Link to="./statistics" onClick={scrollToTop}>
+                    <Link to="/statistics" onClick={scrollToTop}>
                     <button className="cta-button courses-button">Access Lessons</button>
                     </Link>
                 </div>
@@ -99,7 +99,7 @@ const SelfPacedClasses = () => {
                     <img src={Zoology} alt="Zoology" className="img-course"/>
                     <h1>Zoology</h1>
                     <p>In this course we will learn about the field of zoology, some topics include biodiversity, taxonomy, and anatomy.</p>
-                    <Link to="./zoology" onClick={scrollToTop}>
+                    <Link to="/zoology" onClick={scrollToTop}>
                     <button className="cta-button courses-button">Access Lessons</button>
                     </Link>
                 </div>
