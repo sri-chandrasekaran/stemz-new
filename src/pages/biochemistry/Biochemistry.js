c71a58c68fda973a50bdb282622b7f0f7d4b43cb
xiangyshi <xiangy.shi@gmail.com>
2025-04-28 14:21:43 -0700

    fix(refactor): image path build issue

--- src/pages/biochemistry/Biochemistry.js
+++ src/pages/biochemistry/Biochemistry.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Navbar from '../../components/Navbar';
 import HeroOther from '../../components/HeroOther';
 import Footer from '../../components/Footer';
-import Biochemistry from '../../assets/biochem.png'
+import AstronomyImage from '../../assets/astronomy.png'
 import { Link } from 'react-router-dom';
 import '../css/Astronomy.css';
 
@@ -16,7 +16,7 @@ const BiochemistryPage = () => {
     <div>
       <Navbar />
       <HeroOther overlayText="Biochemistry"/>
-      <img src={Biochemistry} alt="Biochemistry" className="course-img"/>
+      <img src={AstronomyImage} alt="Biochemistry" className="course-img"/>
       <div className='course-description'>
         <h2>Recommended Grade Level: 3rd - 6th Grade</h2>
         <h2>Length: 2 Lessons, 1 hour each</h2>
